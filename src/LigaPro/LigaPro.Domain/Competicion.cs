311ed7160d890cfb46b108d29e5f2dc9407e00af
Sofia Bernigola <smbernigola@gmail.com>
2025-10-27 19:09:02 -0300

    Initial project structure and domain models

---
+++ src/LigaPro/LigaPro.Domain/Competicion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LigaPro.Domain
+{
+    public abstract class Competicion
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Deporte { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public string Estado { get; set; } //"Inscripción Abierta", "En Curso", "Finalizado"
+        public List<Equipo> EquiposInscritos { get; set; }
+    }
+}
