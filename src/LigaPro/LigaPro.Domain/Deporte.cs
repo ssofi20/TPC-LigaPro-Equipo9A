0e62c34571039174fcdcba175a266d74364a34e2
Sofi19 <bi40725@gmail.com>
2025-10-29 01:47:52 -0300

    clase deporte

---
+++ src/LigaPro/LigaPro.Domain/Deporte.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LigaPro.Domain
+{
+    internal class Deporte
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int CantJugadores { get; set; }
+    }
+}
