c597cda34053bbe354424611a085ff548f98f8de
Sofi19 <bi40725@gmail.com>
2025-10-29 01:50:43 -0300

    Clase inscripcion

---
+++ src/LigaPro/LigaPro.Domain/Inscripcion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LigaPro.Domain
+{
+    internal class Inscripcion
+    {
+        public int IdEquipo { get; set; }
+        public int IdCompetencia { get; set; }
+
+        public DateTime FechaInscripcion { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
