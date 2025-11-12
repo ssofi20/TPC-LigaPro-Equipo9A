7afbf2ce2788b4f87d87fd6524699efc082cb0f2
Sofia Bernigola <smbernigola@gmail.com>
2025-11-12 17:05:44 -0300

    Replace Panel controls with HtmlGenericControl in navigation
    Updated navigation elements in Site.Master to use HtmlGenericControl (ul) instead of Panel controls for public, player, and organizer navigation. This improves semantic HTML structure and aligns the designer file with the markup changes.

--- src/LigaPro/LigaPro.Web/Site.Master.designer.cs
+++ src/LigaPro/LigaPro.Web/Site.Master.designer.cs
@@ -39,7 +39,7 @@ namespace LigaPro.Web
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel PanelNavegacionPublica;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl PanelNavegacionPublica;
 
         /// <summary>
         /// PanelNavegacionJugador control.
@@ -48,7 +48,7 @@ namespace LigaPro.Web
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel PanelNavegacionJugador;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl PanelNavegacionJugador;
 
         /// <summary>
         /// btnCerrarSesionJugador control.
@@ -66,7 +66,7 @@ namespace LigaPro.Web
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel PanelNavegacionOrganizador;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl PanelNavegacionOrganizador;
 
         /// <summary>
         /// btnCerrarSesionOrganizador control.
